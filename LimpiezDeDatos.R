6a89dd1dabe88de9a50cc974c83f8b0b549a8b83
Mauro-Escritorio <maurogalvanbalca@gmail.com>
2022-08-20 19:26:37 -0300

    Limpieza de las filas I y X de forma manual.

--- LimpiezDeDatos.R
+++ LimpiezDeDatos.R
@@ -11,16 +11,16 @@ stringToFirst4CharsNumeric <- function(y) {
 
 #Esto es para transformar las columnas de anios que tienen letras, a solo los primeros 4 digitos numericos, para asi tener el anio unicamente.
 Encuesta[6] <- apply(Encuesta[6], 2, stringToFirst4CharsNumeric)
-Encuesta[8] <- apply(Encuesta[8], 2, stringToFirst4CharsNumeric)
+Encuesta[7] <- apply(Encuesta[7], 2, stringToFirst4CharsNumeric)
 
-#Elimino la fila que no servia
-Encuesta$...7 <- NULL
 
 #Me quedo con solo los que ya no estudian mas.
 EncuestaYaNoEstudian <- Encuesta[Encuesta[33] == "No, ya no estudio ninguna carrera.", ] 
 
+#Me quedo con solo los que aparentemente siguen estudiando pero no se matricularon.
 EncuestaNoSeMatriculo <- Encuesta[Encuesta[33] == "Si sigo estudiando, solo que no me matricule este año 2021.", ]
 
+#Estudian otra carrera.
 EncuestaAbandonoSigueOtraCarrera <- Encuesta[Encuesta[33] == "Abandone otras carreras pero continúo estudiando otra", ]
 
 #Creacion del excel
