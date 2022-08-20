fc774f65cc62521157365b908f155472a1785563
Mauro-Escritorio <maurogalvanbalca@gmail.com>
2022-08-20 17:42:01 -0300

    Separacion en 3 frames

--- LimpiezDeDatos.R
+++ LimpiezDeDatos.R
@@ -1,20 +1,26 @@
 library(readxl)
-library(dplyr)
+
 
 Encuesta <- read_excel("encuesta.xlsx")
 
-Encuesta$Group <- gsub("[^0-9]","",Encuesta[6])
 
-gsub("[^0-9]","",Encuesta[6])
+stringToFirst4CharsNumeric <- function(y) {
+  substr(as.numeric(gsub("[^0-9]","",y)),1,4)
+}
 
 
 #Esto es para transformar las columnas de anios que tienen letras, a solo los primeros 4 digitos numericos, para asi tener el anio unicamente.
-Encuesta[6] <- apply(Encuesta[6], 2, function(y) as.numeric(gsub("[^0-9]","",substr(y,1,4))))
-Encuesta[8] <- apply(Encuesta[8], 2, function(y) as.numeric(gsub("[^0-9]","",substr(y,1,4))))
+Encuesta[6] <- apply(Encuesta[6], 2, stringToFirst4CharsNumeric)
+Encuesta[8] <- apply(Encuesta[8], 2, stringToFirst4CharsNumeric)
 
+#Elimino la fila que no servia
+Encuesta$...7 <- NULL
 
+#Me quedo con solo los que ya no estudian mas.
+EncuestaYaNoEstudian <- Encuesta[Encuesta[33] == "No, ya no estudio ninguna carrera.", ] 
 
-Encuesta$Group
+EncuestaNoSeMatriculo <- Encuesta[Encuesta[33] == "Si sigo estudiando, solo que no me matricule este año 2021.", ]
 
-#Encuesta$Group
+EncuestaAbandonoSigueOtraCarrera <- Encuesta[Encuesta[33] == "Abandone otras carreras pero continúo estudiando otra", ]
 
+#Creacion del excel
